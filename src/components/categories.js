004bbb37a785a4d2c50bc11a6f6d00799122efbf
unknown <axel.duval@gmail.com>
2021-11-17 16:58:07 +0100

    page recherche

--- src/components/categories.js
+++ src/components/categories.js
@@ -1,87 +1,52 @@
 import React, { useState, useEffect } from 'react';
+import './categories.css'
 
 
 function Categories() {
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [items, setItems] = useState([]);
-  
-    // Remarque : le tableau vide de dépendances [] indique
-    // que useEffect ne s’exécutera qu’une fois, un peu comme
-    // componentDidMount()
-    useEffect(() => {
-      fetch("https://swapi.dev/api/").then(res => res.json()).then((result) => {
-            setIsLoaded(true);
-            setItems(result);
-            // console.log(result);
-          },
-          // Remarque : il faut gérer les erreurs ici plutôt que dans
-          // un bloc catch() afin que nous n’avalions pas les exceptions
-          // dues à de véritables bugs dans les composants.
-          (error) => {
-            setIsLoaded(true);
-            setError(error);
-          }
-        )
-    }, [])
-  
-    if (error) {
-      return <div>Erreur : {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Chargement...</div>;
-    } else {
-        // console.log("return :")
-        // console.log(items);
-        const { borders = [] } =  items;
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
 
-      return ( 
+  // Remarque : le tableau vide de dépendances [] indique
+  // que useEffect ne s’exécutera qu’une fois, un peu comme
+  // componentDidMount()
+  useEffect(() => {
+    fetch("https://swapi.dev/api/").then(res => res.json()).then((result) => {
+      setIsLoaded(true);
+      setItems(result);
+      // console.log(result);
+    },
+      // Remarque : il faut gérer les erreurs ici plutôt que dans
+      // un bloc catch() afin que nous n’avalions pas les exceptions
+      // dues à de véritables bugs dans les composants.
+      (error) => {
+        setIsLoaded(true);
+        setError(error);
+      }
+    )
+  }, [])
 
-        // use borders directly
-          <ul>
-            {borders.map(border => {
-             <li key={border}>{border}</li>;
-            })}
-            
-          </ul>
-          
-        
+  if (error) {
+    return <div>Erreur : {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Chargement...</div>;
+  } else {
+    
+    return (
 
-      );
-    }
+      <div className="row justify-content-center mt-3">
+        {
+        Object.keys(items).map((item, i) => (
+          <div className="card col-3 m-2 p-0" key={i}>
+          <img className="card-img-top" src="https://c2.lestechnophiles.com/www.numerama.com/wp-content/uploads/2018/02/star-wars.jpg?resize=600,300"></img>
+            <div className="card-body text-center p-5">{item}</div>
+          </div>
+        ))
+        }
+      </div>
+
+    )
   }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //     return (
-//       <>
-//         <main>
-//           <h2>Accueil</h2>
-//           <p>You can do this, I believe in you.</p>
-//         </main>
-//       </>
-//     );
-//   }
-
-  export default Categories;
+export default Categories;
--- src/components/recherche.js
+++ src/components/recherche.js
@@ -1,23 +1,67 @@
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import './recherche.css'
 
 
 function Recherche() {
-    return (
-      <>
-        <main>
-          <h2>Recherche</h2>
-          <p>
-            That feels like an existential question, don't you
-            think?
-          </p>
-          
-          <form className="d-flex">
-                        <input className="form-control me-2" type="search" placeholder="Rechercher" aria-label="Search"/>
-                        <button className="btn btn-outline-success" type="submit">Rechercher</button>
-                        </form>
-        </main>
-      </>
-    );
-  }
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+  const [input, setInput] = useState('1');
 
-  export default Recherche;
+  // Remarque : le tableau vide de dépendances [] indique
+  // que useEffect ne s’exécutera qu’une fois, un peu comme
+  // componentDidMount()
+  useEffect(() => {
+    fetch(`https://swapi.dev/api/people/${input}/`).then(res => res.json()).then((result) => {
+      setIsLoaded(true);
+      setItems(result);
+      // console.log(result);
+    },
+      // Remarque : il faut gérer les erreurs ici plutôt que dans
+      // un bloc catch() afin que nous n’avalions pas les exceptions
+      // dues à de véritables bugs dans les composants.
+      (error) => {
+        setIsLoaded(true);
+        setError(error);
+      }
+    )
+  }, [input])
+
+
+  if (error) {
+    return <div>Erreur : {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Chargement...</div>;
+  } else {
+    console.log(items);
+    console.log(input);
+    return (
+
+      <div className="row justify-content-center m-3 p-2">
+
+        <form className="mb-3">
+          <input
+          className="form-control" 
+          type="search" 
+          placeholder="Rechercher un personnage" 
+          aria-label="Search" 
+          value={input}
+          onInput={e => setInput(e.target.value)}
+          />
+        </form>
+
+
+        {
+          Object.keys(items).map((item, i) => (
+            <ul className="m-2" key={i}>
+              <li><b>{item}</b> : {items[item]}</li>
+            </ul>
+          ))
+        }
+      </div>
+
+    )
+  }
+}
+
+export default Recherche;
