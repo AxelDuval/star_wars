031a84e5c2acd0c635544a514395df64e50e6131
AxelDuval <axel.duval@gmail.com>
2021-11-20 14:32:14 +0100

    react gh page

--- src/components/categories.js
+++ src/components/categories.js
@@ -7,18 +7,12 @@ function Categories() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
 
-  // Remarque : le tableau vide de dépendances [] indique
-  // que useEffect ne s’exécutera qu’une fois, un peu comme
-  // componentDidMount()
   useEffect(() => {
     fetch("https://swapi.dev/api/").then(res => res.json()).then((result) => {
       setIsLoaded(true);
       setItems(result);
-      // console.log(result);
     },
-      // Remarque : il faut gérer les erreurs ici plutôt que dans
-      // un bloc catch() afin que nous n’avalions pas les exceptions
-      // dues à de véritables bugs dans les composants.
+
       (error) => {
         setIsLoaded(true);
         setError(error);
@@ -39,7 +33,8 @@ function Categories() {
         Object.keys(items).map((item, i) => (
           <div className="card col-3 m-2 p-0" key={i}>
           <img className="card-img-top" src="https://c2.lestechnophiles.com/www.numerama.com/wp-content/uploads/2018/02/star-wars.jpg?resize=600,300" alt="images"></img>
-            <div className="card-body text-center p-5">{item}</div>
+            <h5 className="card-title text-center p-5">{item}</h5>
+            <button className="btn btn-warning">Voir</button>
           </div>
         ))
         }
